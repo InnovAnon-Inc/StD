cb04da170c9ab9cfe9453efdbeb5a32d26c40dac
Default User <laurence.a.maddox@gmail.com>
2017-10-01 19:09:42 -0500

    backup

--- src/libsimon.c
+++ src/libsimon.c
@@ -18,48 +18,78 @@
 TODO (struct std with RNG + seed etc)
 
 __attribute__ ((const, leaf, nothrow, warn_unused_result))
-int range_int (int min, int max) {
-	return max - min + 1;
+unsigned int range_int (int min, int max) {
+	int ret;
+	assert (min <= max);
+	ret = max - min + 1;
+	assert (ret >= 0);
+	return (unsigned int) ret;
 }
 
 /* https://stackoverflow.com/questions/5008804/generating-random-integer-from-a-range */
 
 __attribute__ ((leaf, nothrow, warn_unused_result))
-int random_range_off_naive1 (int r, int off) {
+int random_range_off_naive1 (unsigned int r, int off) {
+	int ret;
+	assert (r != 0);
+	assert (r <= INT_MAX);
+	/*assert (r <= INT_MAX - off); assert (r + off <= INT_MAX)*/
 	TODO (allow other RNGs)
-	return off + (rand () % r);
+	ret = off + (rand () % (int) r);
+	assert (off <= ret);
+	assert (ret <= off + (int) r);
+	return ret;
 }
 
 /* probabilities are slightly skewed */
 __attribute__ ((nothrow, warn_unused_result))
 int random_range_naive1 (int min, int max) {
 	TODO (allow other RNGs)
-	return random_range_off_naive1 (range (min, max), min);
+	int ret = random_range_off_naive1 (range (min, max), min);
+	assert (min <= ret);
+	assert (ret <= max);
+	return ret;
 }
 
 /* the "skew" is a bit better distributed here */
 __attribute__ ((leaf, nothrow, warn_unused_result))
 int random_range_naive2 (int min, int max) {
-	int     n  = range (min, max);
-	int    tmp = rand ();
-	double ret = (double) tmp / (double) RAND_MAX * (double) n;
-	return (int) ret;
+	unsigned int n = range (min, max);
+	int tmp;
+	double ret;
+	assert (n != 0);
+	tmp = rand ();
+	ret = (double) tmp / (double) RAND_MAX * (double) n;
+	tmp = (int) ret;
+	assert (min <= tmp);
+	assert (tmp <= max);
+	return tmp;
 }
 
 /* warning: there's some sort of effect due to the interaction of this algo
  * and some pRNGs */
 __attribute__ ((leaf, nothrow, warn_unused_result))
-int random_range_off_java (int n, int off) {
-	int r = RAND_MAX % n;
-	int d = RAND_MAX - r;
+int random_range_off_java (unsigned int n, int off) {
+	int r;
+	int d;
 	int x;
+	assert (n != 0);
+	assert (n <= INT_MAX);
+	r = RAND_MAX % (int) n;
+	d = RAND_MAX - (int) r;
 	do x = rand ();
 	while (x >= d);
-	return off + x % n;
+	r = off + x % n;
+	assert (off <= r);
+	assert (r <= off + (int) n);
+	return r;
 }
 __attribute__ ((nothrow, warn_unused_result))
 int random_range_java (int min, int max) {
-	return random_range_off_java (range (min, max), min);
+	int ret = random_range_off_java (range (min, max), min);
+	assert (min <= ret);
+	assert (ret <= max);
+	return ret;
 }
 
 __attribute__ ((nonnull (1, 3), nothrow))
@@ -109,6 +139,12 @@ size_t random_range_java_size_t (size_t min, size_t max) {
 	do ez_random_ranges ((int *restrict) x, sizeof (size_t), 0, CHAR_MAX);
 	while (*(size_t *) x >= d);
 	return min + *(size_t *) x % n;
+}
+
+size_t random_range_java_size_t2 (size_t min, size_t max) {
+	assert (min < max);
+	assert
+	return random_range_java ();
 }
 
 __attribute__ ((nonnull (1, 3, 4, 5), nothrow))
